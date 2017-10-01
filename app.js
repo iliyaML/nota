97d7a7bd2473f528d8cb21fcd82a45bdf96215b9
iliyaml <mohamad.i.mohamad.lokman@vanderbilt.edu>
2017-10-01 08:43:32 -0500

    Added mongoose connection

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
+// import dependencies
 const express = require('express');
 const app = express();
-
+const mongoose = require('mongoose');
 const exphbs = require('express-handlebars');
 
 const PORT = 5000;
+
+// map global promise - get rid of warning
+// remove mongoose deprecated promise
+mongoose.Promise = global.Promise;
+
+// set up mongoose/mongodb connection
+mongoose.connect('mongodb://localhost/nota', {
+  useMongoClient: true // to prevent warning
+}).then(() => console.log('MongoDB connected...'))
+.catch(err => console.log(err));
 
 // handlebars middleware
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
