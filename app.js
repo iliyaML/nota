bfba8375f97a591543c7087bc3dc22b3df719584
iliyaml <mohamad.i.mohamad.lokman@vanderbilt.edu>
2017-09-30 22:31:49 -0500

    Added index and about pages

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const app = express();
+
 const PORT = 5000;
 
+// index route
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// about route
+app.get('/about', (req, res) => {
+  res.send('About');
+});
+
+// server listening
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
