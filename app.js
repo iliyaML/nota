408e61a4c9f9824615b2dfb45f0084d82a03ef0f
iliyaml <mohamad.i.mohamad.lokman@vanderbilt.edu>
2017-09-30 22:52:35 -0500

    Added handlebars template engine

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 const express = require('express');
 const app = express();
 
+const exphbs = require('express-handlebars');
+
 const PORT = 5000;
+
+// handlebars middleware
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
 // index route
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.render('index', {
+    title: 'Index'
+  });
 });
 
 // about route
 app.get('/about', (req, res) => {
-  res.send('About');
+  res.render('about');
 });
 
 // server listening
