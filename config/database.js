b5c245967316a9e36ce605609517f59582c42f60
iliyaml <mohamad.i.mohamad.lokman@vanderbilt.edu>
2017-10-01 14:06:12 -0500

    Changed db config

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 if(process.env.NODE_ENV == 'production'){
   module.exports = {
-    mongoURI: 'mongodb://<dbuser>:<dbpassword>@ds155674.mlab.com:55674/iliya-nota'
+    mongoURI: 'mongodb://mohamami:test123@ds155674.mlab.com:55674/iliya-nota'
   };
 } else {
   module.exports = {
